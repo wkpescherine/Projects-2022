29b97f1b8321146513fc39b996674cfd9264fd38
wkpescherine <wkpescherine@gmail.com>
2021-12-17 00:10:11 -0500

    update files and check if working as intended

---
+++ app/src/main/java/com/example/ascension/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ascension;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+}
