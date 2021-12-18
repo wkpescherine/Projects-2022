431f03d29af83eb14e76dcabe5a9f7aab19841c8
wkpescherine <wkpescherine@gmail.com>
2021-12-17 23:11:45 -0500

    Adding in the character load and create screens basic setup and transition between them

---
+++ app/src/main/java/com/example/ascension/CreateCharacter.java
@@ -0,0 +1,14 @@
+package com.example.ascension;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateCharacter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_character);
+    }
+}
--- app/src/main/java/com/example/ascension/MainActivity.java
+++ app/src/main/java/com/example/ascension/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ascension;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.view.View;
+import android.content.Intent;
 
 import android.os.Bundle;
 
@@ -14,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+    }
+
+    public void loadCharScreen(View v){
+        Intent intent = new Intent(this, LoadCharacter.class);
+        startActivity(intent);
+    }
+
+    public void createCharScreen(View v){
+        Intent intent = new Intent(this, CreateCharacter.class);
+        startActivity(intent);
     }
 }
