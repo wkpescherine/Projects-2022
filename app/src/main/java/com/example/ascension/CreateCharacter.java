5feece3a58831071e53f872c7d1a7faf2828f59a
wkpescherine <wkpescherine@gmail.com>
2021-12-18 23:10:22 -0500

    Adding in the character load and create screens basic setup and transition between them

--- app/src/main/java/com/example/ascension/CreateCharacter.java
+++ app/src/main/java/com/example/ascension/CreateCharacter.java
@@ -1,14 +1,35 @@
 package com.example.ascension;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
 import android.os.Bundle;
 
 public class CreateCharacter extends AppCompatActivity {
+    String charStyle = "";
+    String charRace = "";
+    String charClass = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_character);
     }
+
+    public void handleStyle(View v){
+        charStyle ="something";
+    }
+
+    public void handleRace(View v){
+        charRace="whatever";
+    }
+
+    public void handleClass(View v){
+        charClass="IDK";
+    }
+
+    public void backToMain(View v){ }
+
+    public void loadSkills(View v){ }
+
 }
