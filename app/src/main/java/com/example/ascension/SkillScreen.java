7b5483658aaee632c9e868cad914edf0adca0eba
wkpescherine <wkpescherine@gmail.com>
2021-12-20 23:19:16 -0500

    adding in more logic for charsheet handling across multiple activities

--- app/src/main/java/com/example/ascension/SkillScreen.java
+++ app/src/main/java/com/example/ascension/SkillScreen.java
@@ -3,8 +3,10 @@ package com.example.ascension;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SkillScreen extends AppCompatActivity {
+    CharDataConfig charSheet = new CharDataConfig();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
