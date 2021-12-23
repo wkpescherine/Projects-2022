0c22f0c7db8513914093b09b323995d74f8e35da
wkpescherine <wkpescherine@gmail.com>
2021-12-22 23:06:08 -0500

    Created 3 base SRC with stats and also char sheet stats handling

--- app/src/main/java/com/example/ascension/CharDataConfig.java
+++ app/src/main/java/com/example/ascension/CharDataConfig.java
@@ -13,7 +13,13 @@ public class CharDataConfig {
     int styleDiabolic [] = {1,4,2,1,3,1};
     int styleDivine [] = {1,2,4,1,3,1};
     int raceHuman [] = {2,2,2,2,2,2};
+    int raceElven [] = {1,3,1,3,3,1};
+    int raceDwarven [] = {3,1,1,1,1,5};
     int classWarrior [] = {2,0,0,1,0,3};
+    int classMage [] = {0,2,1,0,3,0};
+    int classThief [] = {1,0,0,2,2,1};
+    static int charStats [] = {0,0,0,0,0,0};
+    static int charBonus [] = {0,0,0,0,0,0};
 
     public void updateStyle( String choice){
         charStyle = choice;
@@ -26,6 +32,7 @@ public class CharDataConfig {
         if(charStyle.equals("Divine")){
             styleStats = styleDivine;
         }
+        statsTotal();
     }
 
     public void updateRace( String choice){
@@ -33,6 +40,13 @@ public class CharDataConfig {
         if(charRace.equals("Human")){
             raceStats = raceHuman;
         }
+        if(charRace.equals("Elven")){
+            raceStats = raceElven;
+        }
+        if(charRace.equals("Dwarven")){
+            raceStats = raceDwarven;
+        }
+        statsTotal();
     }
 
     public void updateClass( String choice){
@@ -40,6 +54,13 @@ public class CharDataConfig {
         if(charClass.equals("Warrior")){
             classStats = classWarrior;
         }
+        if(charClass.equals("Mage")){
+            classStats = classMage;
+        }
+        if(charClass.equals("Thief")){
+            classStats = classThief;
+        }
+        statsTotal();
     }
 
     public void resetUI(){
@@ -47,5 +68,25 @@ public class CharDataConfig {
         charRace = "";
         charClass = "";
         charName = "";
+    }
+
+    public void statsTotal(){
+        charStats[0] = styleStats[0]+raceStats[0]+classStats[0];
+        charStats[1] = styleStats[1]+raceStats[1]+classStats[1];
+        charStats[2] = styleStats[2]+raceStats[2]+classStats[2];
+        charStats[3] = styleStats[3]+raceStats[3]+classStats[3];
+        charStats[4] = styleStats[4]+raceStats[4]+classStats[4];
+        charStats[5] = styleStats[5]+raceStats[5]+classStats[5];
+        statsBonus();
+    }
+
+    public void statsBonus(){
+        for(int a = 0; a < 6; a++){
+            int bonusValue = 0;
+            for(int b = 0; b < charStats[a]; b++){
+                bonusValue +=1;
+            }
+            charBonus[a] = bonusValue;
+        }
     }
 }
