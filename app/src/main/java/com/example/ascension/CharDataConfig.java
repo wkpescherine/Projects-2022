b687bbd3addd7ac1ad9307cd27c5681fcfea9b9e
wkpescherine <wkpescherine@gmail.com>
2021-12-23 22:35:21 -0500

    Create Char UI updated Added 3 new SRC as well as stats for them Fixed the bonus calculation

--- app/src/main/java/com/example/ascension/CharDataConfig.java
+++ app/src/main/java/com/example/ascension/CharDataConfig.java
@@ -10,14 +10,23 @@ public class CharDataConfig {
     int raceStats[] = {0,0,0,0,0,0};
     int classStats[] = {0,0,0,0,0,0};
     int styleSavage [] = {3,0,0,2,0,1};
-    int styleDiabolic [] = {1,4,2,1,3,1};
-    int styleDivine [] = {1,2,4,1,3,1};
+    int styleDiabolic [] = {0,3,0,1,1,1};
+    int styleDivine [] = {0,0,3,1,1,1};
+    int styleStout [] = {0,0,1,1,1,4};
+    int styleDemonic [] = {0,2,0,1,1,2};
+    int stylePure [] = {0,0,4,0,0,2};
     int raceHuman [] = {2,2,2,2,2,2};
     int raceElven [] = {1,3,1,3,3,1};
     int raceDwarven [] = {3,1,1,1,1,5};
+    int raceUndead [] = {4,1,1,1,1,4};
+    int raceGoblin [] = {1,1,1,4,4,1};
+    int raceOgre [] = {6,1,1,1,1,2};
     int classWarrior [] = {2,0,0,1,0,3};
     int classMage [] = {0,2,1,0,3,0};
     int classThief [] = {1,0,0,2,2,1};
+    int classBerzerker [] = {4,0,0,1,1,0};
+    int classPriest [] = {0,0,3,1,1,1};
+    int classRanger [] = {2,0,0,2,2,0};
     static int charStats [] = {0,0,0,0,0,0};
     static int charBonus [] = {0,0,0,0,0,0};
 
@@ -31,6 +40,15 @@ public class CharDataConfig {
         }
         if(charStyle.equals("Divine")){
             styleStats = styleDivine;
+        }
+        if(charStyle.equals("Stout")){
+            styleStats = styleStout;
+        }
+        if(charStyle.equals("Demonic")){
+            styleStats = styleDemonic;
+        }
+        if(charStyle.equals("Pure")){
+            styleStats = stylePure;
         }
         statsTotal();
     }
@@ -46,6 +64,15 @@ public class CharDataConfig {
         if(charRace.equals("Dwarven")){
             raceStats = raceDwarven;
         }
+        if(charRace.equals("Undead")){
+            raceStats = raceUndead;
+        }
+        if(charRace.equals("Goblin")){
+            raceStats = raceGoblin;
+        }
+        if(charRace.equals("Ogre")){
+            raceStats = raceOgre;
+        }
         statsTotal();
     }
 
@@ -59,6 +86,15 @@ public class CharDataConfig {
         }
         if(charClass.equals("Thief")){
             classStats = classThief;
+        }
+        if(charClass.equals("Berzerker")){
+            classStats = classBerzerker;
+        }
+        if(charClass.equals("Priest")){
+            classStats = classPriest;
+        }
+        if(charClass.equals("Ranger")){
+            classStats = classRanger;
         }
         statsTotal();
     }
@@ -84,7 +120,9 @@ public class CharDataConfig {
         for(int a = 0; a < 6; a++){
             int bonusValue = 0;
             for(int b = 0; b < charStats[a]; b++){
-                bonusValue +=1;
+                if(b+bonusValue <= charStats[a] ){
+                    bonusValue = b;
+                }
             }
             charBonus[a] = bonusValue;
         }
