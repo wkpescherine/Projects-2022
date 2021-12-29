1a5d48074b5096d656fcd6b25e76c5ce2b114ce5
wkpescherine <wkpescherine@gmail.com>
2021-12-28 23:59:50 -0500

    Added 3 SRC to Create Screen

---
+++ app/src/main/java/com/example/ascension/CharFileHandler.java
@@ -0,0 +1,14 @@
+package com.example.ascension;
+
+import android.widget.LinearLayout;
+
+public class CharFileHandler {
+    CharDataConfig config = new CharDataConfig();
+
+    public void writeCharToFile(){}
+
+    public void readCharFromFile(){
+
+    }
+}
+
--- app/src/main/java/com/example/ascension/LoadCharacter.java
+++ app/src/main/java/com/example/ascension/LoadCharacter.java
@@ -2,13 +2,29 @@ package com.example.ascension;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LoadCharacter extends AppCompatActivity {
+    CharDataConfig config = new CharDataConfig();
+    CharFileHandler handleFile = new CharFileHandler();
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_character);
+
+        handleFile.readCharFromFile();
+    }
+
+    public void buildCharsToLoad(){
+        LinearLayout parent = findViewById(R.id.subContent);
+
+        LinearLayout savedChar = new LinearLayout();
+
+        TextView tv1 = new TextView();
     }
 }
