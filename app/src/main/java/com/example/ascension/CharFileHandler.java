643445856f2ee3153cfc1df1f64f3894e97fb73c
wkpescherine <wkpescherine@gmail.com>
2021-12-30 23:08:28 -0500

    Trying to resolve the write for saving games

--- app/src/main/java/com/example/ascension/CharFileHandler.java
+++ app/src/main/java/com/example/ascension/CharFileHandler.java
@@ -1,22 +1,33 @@
 package com.example.ascension;
 
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class CharFileHandler {
+    private static final String TAG = "";
     CharDataConfig config = new CharDataConfig();
 
     public void writeCharToFile(){
         try{
-        FileWriter myWriter = new FileWriter("savedGames.txt");
-        BufferedWriter bWriter = new BufferedWriter(myWriter);
-        PrintWriter pWriter = new PrintWriter(bWriter);
-        pWriter.write("test");
-        pWriter.close();} catch(IOException e){
+            FileWriter myWriter = new FileWriter("SavedGames.txt", true);
+            BufferedWriter bWriter = new BufferedWriter(myWriter);
+            PrintWriter pWriter = new PrintWriter(bWriter);
+            pWriter.write("Test");
+            pWriter.close();
+            System.out.print("Current Game Saved");
+        }catch(IOException e){
             e.printStackTrace();
         }
     }
