e49629b759fcd32a6adf797fd4f0336b1cf5303c
wkpescherine <wkpescherine@gmail.com>
2022-01-03 01:04:44 -0500

    Added 3 QMT to start building our Gear Building

--- app/src/main/java/com/example/ascension/GearBuilder.java
+++ app/src/main/java/com/example/ascension/GearBuilder.java
@@ -6,23 +6,45 @@ public class GearBuilder {
     String gearQuality = "";
     String gearMaterial = "";
     String gearType = "";
-    int upper = 13;
+    int upper = 3;
     //damage, spd, AF
     int gearValues [] = {0,0,0};
     int tempValues [][] ={{0,0,0}, {0,0,0}, {0,0,0}};
-    String gearQualityNames [] = {"Pitted", "Rusty", "Poor"};
-    int gearQualityValues [][] = {{1,0,0}, {2,0,0}, {3,0,0}};
+    String gearWeaponQualityNames [] = {"Pitted", "Rusty", "Poor"};
+    String gearWeaponMaterialNames [] = {"Iron","Steel", "Bronze"};
+    int gearWeaponQualityValues [][] = {{1,0,0}, {2,0,0}, {3,0,0}};
+    int gearWeaponMaterialValues [][] = {{1,0,0}, {2,0,0}, {3,0,0}};
 
     public void gearQuality(){
         Random rand = new Random();
         int gearInt = rand.nextInt(upper)+1;
-        gearQuality = gearQualityNames[gearInt];
-        tempValues [0] = gearQualityValues[gearInt];
+        gearQuality = gearWeaponQualityNames[gearInt];
+        tempValues [0] = gearWeaponQualityValues[gearInt];
     }
 
-    public void gearMaterial(){}
+    public void gearMaterial(){
+        Random rand = new Random();
+        int gearInt2 = rand.nextInt(upper)+1;
+        gearMaterial = gearWeaponMaterialNames[gearInt2];
+        tempValues [1] = gearWeaponMaterialValues[gearInt2];
+    }
 
-    public void gearType(){
+    public void gearType(String Type){
         gearQuality();
+        gearMaterial();
+        // 1 : Weapon
+        if(Type.equals("Dagger")){
+            gearType = Type
+            int daggerVales [] = {1,5,0};
+            tempValues [2] = daggerVales;
+        }
+        // 2 : Chest
+        if(gearInt3 == 2){
+
+        }
+        // 3 : Leggings
+        if(gearInt3 == 3){
+
+        }
     }
 }
