551449a6c3cf110916af2dc3425541e99371433a
wkpescherine <wkpescherine@gmail.com>
2022-01-02 23:57:58 -0500

    Added Gear quality builder

--- app/src/main/java/com/example/ascension/GearBuilder.java
+++ app/src/main/java/com/example/ascension/GearBuilder.java
@@ -1,12 +1,28 @@
 package com.example.ascension;
 
+import java.util.Random;
+
 public class GearBuilder {
+    String gearQuality = "";
+    String gearMaterial = "";
+    String gearType = "";
+    int upper = 13;
+    //damage, spd, AF
+    int gearValues [] = {0,0,0};
+    int tempValues [][] ={{0,0,0}, {0,0,0}, {0,0,0}};
+    String gearQualityNames [] = {"Pitted", "Rusty", "Poor"};
+    int gearQualityValues [][] = {{1,0,0}, {2,0,0}, {3,0,0}};
 
-    public void gearQuality(String gearQua){
-
+    public void gearQuality(){
+        Random rand = new Random();
+        int gearInt = rand.nextInt(upper)+1;
+        gearQuality = gearQualityNames[gearInt];
+        tempValues [0] = gearQualityValues[gearInt];
     }
 
     public void gearMaterial(){}
 
-    public void gearType(){}
+    public void gearType(){
+        gearQuality();
+    }
 }
