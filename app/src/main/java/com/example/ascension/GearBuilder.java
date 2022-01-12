f9e8da61b669b5a51aa1a698249f57151ef06e0b
wkpescherine <wkpescherine@gmail.com>
2022-01-11 23:54:21 -0500

    minor bug fix

--- app/src/main/java/com/example/ascension/GearBuilder.java
+++ app/src/main/java/com/example/ascension/GearBuilder.java
@@ -12,7 +12,7 @@ public class GearBuilder {
     int [][]tempValues  = {{0,0,0}, {0,0,0}, {0,0,0}};
     String [] gearWeaponQualityNames  = {"Pitted", "Rusty", "Poor", "Weak", "Basic", "Excellent", "Superior","Perfect", "Legendary", "Mythical"};
     String [] gearWeaponMaterialNames = {"Iron","Steel", "Bronze", "?????","?????", "Damascues","Titanium","Mithril", "?????", "Astral"};
-    String [] gearWeaponTypes         = {"Dagger", "Short Sword", "Staff", "Long Sword", "Bastard Sword" "Axed", "War Hammer", "Maul", "Claymore"};
+    String [] gearWeaponTypes         = {"Dagger", "Short Sword", "Staff", "Long Sword", "Bastard Sword", "Axed", "War Hammer", "Maul", "Claymore"};
     int [][] gearWeaponQualityValues  = {{1,0,0}, {2,0,0}, {3,0,0}, {4,1,0}, {5,1,0}, {6,1,0}, {7,2,0}, {8,2,0}, {9,2,0}, {10,3,0}};
     int [][] gearWeaponMaterialValues = {{1,0,0}, {2,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}};
     int [][] gearWeaponValues         = {{1,5,0}, {2,3,0}, {1,3,0}, {5,3,0}, {7,2,0}, {6,2,0}, {8,2,0}, {14,0,0}, {12,1,0}};
