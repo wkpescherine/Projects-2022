8a3c23618465c7d10f3ecc8a11cfdceffa392739
wkpescherine <wkpescherine@gmail.com>
2022-01-13 23:49:40 -0500

    Updated Gear Builder to be able to reandomize gear Updating also for starter gear Adjusting some logic with char sheet and starting

--- app/src/main/java/com/example/ascension/GearBuilder.java
+++ app/src/main/java/com/example/ascension/GearBuilder.java
@@ -6,6 +6,7 @@ public class GearBuilder {
     String gearQuality = "";
     String gearMaterial = "";
     String gearType = "";
+    String item = "";
     //damage, spd, AF
     int []gearValues    = {0,0,0};
     int [][]tempValues  = {{0,0,0}, {0,0,0}, {0,0,0}};
@@ -16,18 +17,22 @@ public class GearBuilder {
     int [][] gearWeaponMaterialValues = {{1,0,0}, {2,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}, {3,0,0}};
     int [][] gearWeaponValues         = {{1,5,0}, {2,3,0}, {1,3,0}, {5,3,0}, {7,2,0}, {6,2,0}, {8,2,0}, {14,0,0}, {12,1,0}};
     //Armor Section
-    String [] gearArmorQualitNames    = {"Tattered", "Worn","Ragged","Weakened", "Basic","Refined", "Excellent","Superior", "Legendary", "Mythical"};
+    String [] gearArmorQualityNames   = {"Tattered", "Worn","Ragged","Weakened", "Basic","Refined", "Excellent","Superior", "Legendary", "Mythical"};
     String [] gearArmorMaterialNames  = {"Wool","Cloth", "Hide", "Leather", "Studded", "Banded", "Chain", "Plated", "Etheral", "Astral"};
-    String [] gearArmorTypes          = {"Helm", "Chest", "Legs", "Boots", "Shoulder", "Arms", "Gloves", "Buckler", "Kite SHield", "Yower Shield"};
+    String [] gearArmorTypesNames     = {"Helm", "Chest", "Legs", "Boots", "Shoulder", "Arms", "Gloves", "Buckler", "Kite SHield", "Yower Shield"};
     int [][] armorQualityValues       = {{0,0,1},{0,0,2},{0,0,3},{0,0,4},{0,0,5},{0,0,6},{0,0,7},{0,0,8},{0,0,9},{0,0,10}};
     int [][] armorMaterialValues      = {{0,0,1},{0,0,2},{0,0,3},{0,0,4},{0,0,6},{0,0,8},{0,0,10},{0,0,13},{0,0,16},{0,0,20}};
     int [][] armorTypeValues          = {{0,0,1},{0,0,5},{0,0,3},{0,0,1},{0,0,1},{0,0,4},{0,0,1},{0,0,1},{0,0,4},{0,0,10}};
 
     public int randGearPicker(){
         int upper = 1111111111;
+        int lower = 1;
         int selection = 0;
         Random rand = new Random();
         int gearInt = rand.nextInt(upper);
+        int itemInt = rand.nextInt(lower);
+        if(itemInt == 0){ item = "weapon"}
+        else{ item == "other"}
         if(gearInt > 0 && gearInt<=1000000000){selection = 0;}
         if(gearInt >= 1000000001 && gearInt<=1100000000){selection = 1;}
         if(gearInt >= 1100000001 && gearInt<=1110000000){selection = 2;}
@@ -41,33 +46,56 @@ public class GearBuilder {
         return selection;
     }
 
-    public void gearQuality(){
-        int value = randGearPicker();
-        gearQuality = gearWeaponQualityNames[value];
-        tempValues [0] = gearWeaponQualityValues[value];
+    public void gearQuality(String item){
+        if(item.equals("weapon")){
+            int value = randGearPicker();
+            gearQuality = gearWeaponQualityNames[value];
+            tempValues [0] = gearWeaponQualityValues[value];
+        }  else {
+            int value = randGearPicker();
+            gearQuality = gearArmorQualityNames[value];
+            tempValues [0] = armorQualityValues[value];
+        }
     }
 
-    public void gearMaterial(){
-        int value = randGearPicker();
-        gearMaterial = gearWeaponMaterialNames[value];
-        tempValues [1] = gearWeaponMaterialValues[value];
+    public void gearMaterial(String item){
+        if(item.equals("weapon")){
+            int value = randGearPicker();
+            gearMaterial = gearWeaponMaterialNames[value];
+            tempValues [1] = gearWeaponMaterialValues[value];
+        } else {
+            int value = randGearPicker();
+            gearMaterial = gearArmorMaterialNames[value];
+            tempValues [1] = armorMaterialValues[value];
+        }
     }
 
-    public void gearType(String Type){
-        gearQuality();
-        gearMaterial();
-        int value = randGearPicker();
-        gearType = gearWeaponTypes[value];
-        tempValues[2] = gearWeaponValues[value];
-    }
-
-    public void starterGear(){
-
+    public void gearType(String item){
+        if(item.equals("weapon")){
+            int value = randGearPicker();
+            gearType = gearWeaponTypes[value];
+            tempValues[2] = gearWeaponValues[value];
+        } else {
+            int value = randGearPicker();
+            gearMaterial = gearArmorTypesNames[value];
+            tempValues [2] = armorTypeValues[value];
+        }
     }
 
     public void resetGearValues(){
         gearQuality = "";
         gearMaterial = "";
         gearType = "";
+    }
+
+    public void buildItem(){
+        gearMaterial();
+        gearQuality();
+        gearType();
+        resetGearValues();
+    }
+
+    public void starterGear(){
+
     }
 }
